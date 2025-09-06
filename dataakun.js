f9cd84163642abd67e426bfa25f3932625f60648
DitxQT99 <alokkellyhamil@gmail.com>
2025-09-06 19:00:13 +0700

    Add files via upload

---
+++ dataakun.js
@@ -0,0 +1,17 @@
+var accounts = [
+  {
+    "username": "Ditx",
+    "password": "Ditx",
+    "expiryDate": null
+  },
+  {
+    "username": "user1",
+    "password": "password1",
+    "expiryDate": "2023-12-31"
+  },
+  {
+    "username": "user2",
+    "password": "password2",
+    "expiryDate": "2024-01-15"
+  }
+];
